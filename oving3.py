c902a1a912f98f857ad33defb946df868bcaaee3
Per Haagensen <pdhaagensen@gmail.com>
2019-10-07 16:50:16 +0200

    first commit

---
+++ oving3.py
@@ -0,0 +1,55 @@
+import random
+import codecs
+import gensim
+import nltk
+import copy
+
+file = "/Users/perhaagensen/PycharmProjects/tdt4117Oving3/pg3300.txt"
+file2 = "/Users/perhaagensen/PycharmProjects/tdt4117Oving3/pg3300_2.txt"
+
+random.seed(123)
+
+#Task 1.1
+#This function returns the opened file. File is set to read only
+def openFile(file):
+    return codecs.open(file, "r", "utf-8")
+
+#Task 1.2
+#This function splits up the text file in to paragraphs, and adds the, to an array.
+def paragraph(file):
+    fil = openFile(file)
+    paragrafer = []
+    paragraf = ""
+    for line in fil.readlines():
+        paragraf += line
+        if line.isspace():
+            if line != "":
+                paragrafer.append(paragraf)
+            paragraf = ""
+            continue
+    return paragrafer
+
+#Task 1.3
+#This function takes in a word, and returns an updated list  where the word
+#is filtered out.
+def wordRemover(word):
+    paragrafListe = []
+    for p in paragraph(file):
+        if word.casefold() not in p.casefold():
+            paragrafListe.append(p)
+    return paragrafListe
+
+
+
+
+paragraph(file)
+
+wordRemover("Gutenberg")
+
+
+
+
+
+
+
+
