394f6c1ad2b0e3506910437a7511a53d0f60e834
Per Haagensen <pdhaagensen@gmail.com>
2019-10-15 12:01:43 +0200

    Lot of shitfixes

--- helpers.py
+++ helpers.py
@@ -1,8 +1,128 @@
 import random
 import codecs
 import gensim
-import nltk
 import copy
 import string
 from nltk.stem.porter import PorterStemmer
+from nltk.probability import FreqDist
+
+file = "/Users/perhaagensen/PycharmProjects/tdt4117Oving3/pg3300.txt"
+#file2 = "/Users/perhaagensen/PycharmProjects/tdt4117Oving3/pg3300_2.txt"
+
+stemmer = PorterStemmer()
+freqDist = FreqDist()
+random.seed(123)
+
+#Task 1.1
+#This function returns the opened file. File is set to read only.
+#Have to find an another way to modify the file's content.
+
+
+def open_file(file):
+    return codecs.open(file, "r", "utf-8")
+
+#Task 1.2
+#The method paragrapgh(), splits up the given books up to paragraphs. This functions
+#checks if a line is full of spaces, and adds everything before these lines into
+#paragrapgs
+
+
+def paragraph(file):
+    fil = open_file(file)
+    paragrafer = []
+    paragraf = ""
+    for line in fil.readlines():
+        paragraf += line
+        if line.isspace():
+            if line != "":
+                paragrafer.append(paragraf)
+            paragraf = ""
+            continue
+    return paragrafer
+
+#Task 1.3
+#This function takes in a word, and returns an updated list  where the word
+#is filtered out.
+
+
+def word_remover(word):
+    paragrafListe = []
+    for p in paragraph(file):
+        if word.casefold() not in p.casefold():
+            paragrafListe.append(p)
+    return paragrafListe
+
+
+#Task 1.4
+
+
+def tokener(paragraf):
+    tokened_paragrafer = []
+    for i, avsnitt in enumerate(paragraf):
+        paragraf[i] = avsnitt.split(" ")
+    return paragraf
+
+
+#Task 1.5
+
+
+def lower_case(paragraf):
+    paragrafer = []
+    for avsnitt in paragraf:
+        ord_lower = []
+        for ord in avsnitt:
+            ord_lower.append(ord.lower())
+        paragrafer.append(ord_lower)
+    return paragrafer
+
+def remove_punc(paragraf):
+    paras = []
+    #goes through each word in the paragraph and removes whitespace, etc.
+    for p in paragraf:
+        words = []
+        for word in p:
+            temp = replace_multiple(word, ['.',',',';',':','\r\n'], ' ')
+            templist = temp.split(' ')
+            for i in templist:
+                if i == '':
+                    continue
+                else:
+                    words.append(i)
+        paras.append(words)
+    return paras
+
+def replace_multiple(word, toBeRemoved, res):
+    for elem in toBeRemoved:
+        if elem in word:
+            word = word.replace(elem, res)
+    return word
+
+
+#Task 1.6
+
+def stem(paragraphs):
+    stemmed_paragraphs = []
+    #for each paragraph, stem each word in the paragraph
+    for p in paragraphs:
+        stemmed_words = []
+        for word in p:
+            stemmed_words.append(stemmer.stem(word))
+        stemmed_paragraphs.append(stemmed_words)
+    return stemmed_paragraphs
+
+
+liste = paragraph(file)
+
+liste = word_remover("Gutenberg")
+
+liste = tokener(liste)
+
+liste = lower_case(liste)
+
+liste = remove_punc(liste)
+
+liste = stem(liste)
+
+
+print(liste[1])
 
